26a67388fa519a9c12f79091294bb7aba5be0513
ShintaroIwai <iwaishintaro25@gmail.com>
2025-08-18 23:26:21 +0300

    parts12-01 and 12-02

--- part12-01_remaining_stock/src/remaining_stock.py
+++ part12-01_remaining_stock/src/remaining_stock.py
@@ -1,1 +1,12 @@
 # Write your solution here:
+def price(item: tuple):
+    return item[2]
+
+def sort_by_remaining_stock(items: list):
+    return sorted(items, key=price)
+
+if __name__ == "__main__":
+    products = [("banana", 5.95, 12), ("apple", 3.95, 3), ("orange", 4.50, 2), ("watermelon", 4.95, 22)]
+
+    for product in sort_by_remaining_stock(products):
+        print(f"{product[0]} {product[2]} pcs")
--- part12-02_seasons/src/seasons.py
+++ part12-02_seasons/src/seasons.py
@@ -1,1 +1,12 @@
 # Write your solution here:
+def return_season(show: dict):
+    return show["seasons"]
+
+def sort_by_seasons(items: list):
+    return sorted(items, key=return_season)
+
+if __name__ == "__main__":
+    shows = [{ "name": "Dexter", "rating" : 8.6, "seasons":9 }, { "name": "Friends", "rating" : 8.9, "seasons":10 },  { "name": "Simpsons", "rating" : 8.7, "seasons":32 }  ]
+
+    for show in sort_by_seasons(shows):
+        print(f"{show['name']} {show['seasons']} seasons")
