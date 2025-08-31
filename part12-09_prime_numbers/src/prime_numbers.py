c7001d4594b2f2d98f910ca6230fdccdb12f81a3
ShintaroIwai <iwaishintaro25@gmail.com>
2025-08-31 16:22:42 +0300

    310825 part 2

--- part12-09_prime_numbers/src/prime_numbers.py
+++ part12-09_prime_numbers/src/prime_numbers.py
@@ -1,1 +1,21 @@
 # Write your solution here
+def is_prime(number: int):
+    if number < 2:
+        return False
+    for i in range(2, number):
+        if number % i == 0:
+            return False
+    return True
+
+def prime_numbers():
+    number = 2
+    while True:
+        if is_prime(number):
+            yield number
+        number += 1
+            
+if __name__ == "__main__":
+    # print(is_prime(5))
+    numbers = prime_numbers()
+    for i in range(8):
+        print(next(numbers))
