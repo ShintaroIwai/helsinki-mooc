1d5dc83b2644227b83c2896845b7b338aa56935f
ShintaroIwai <iwaishintaro25@gmail.com>
2025-08-31 12:40:41 +0300

    310825

--- part12-08_even_numbers/src/even_numbers.py
+++ part12-08_even_numbers/src/even_numbers.py
@@ -1,1 +1,15 @@
 # Write your solution here
+def even_numbers(beginning: int, maximum: int):
+    if beginning % 2 == 0:
+        number = beginning
+    else:
+        number = beginning + 1
+    while number <= maximum:
+        yield number
+        number += 2
+
+if __name__ == "__main__":
+    numbers = even_numbers(2, 10)
+    # print(type(numbers))
+    for number in numbers:
+        print(number)
