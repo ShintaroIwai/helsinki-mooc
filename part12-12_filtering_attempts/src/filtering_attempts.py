2126eabfd34f299dab9ddc7d053eab6dc866ed18
ShintaroIwai <iwaishintaro25@gmail.com>
2025-12-07 00:22:15 +0200

    071225

--- part12-12_filtering_attempts/src/filtering_attempts.py
+++ part12-12_filtering_attempts/src/filtering_attempts.py
@@ -6,5 +6,42 @@ class CourseAttempt:
 
     def __str__(self):
         return f"{self.student_name}, grade for the course {self.course_name} {self.grade}"
+    
+def accepted(attempts: list):
+    return list(filter(lambda x: x.grade >= 1, attempts))
+
+def attempts_with_grade(attempts: list, grade: int):
+    return list(filter(lambda n: n.grade == grade, attempts))
+
+def passed_students(attempts: list, course: str):
+    grade_filter = list(filter(lambda k: k.grade > 0, attempts))
+    course_filter = list(filter(lambda t: t.course_name == course, grade_filter))
+    return sorted(list(map(lambda m: m.student_name, course_filter)))
+    
+if __name__ == "__main__":
+    # part 1
+    # s1 = CourseAttempt("Peter Python", "Introduction to Programming", 3)
+    # s2 = CourseAttempt("Olivia C. Objective", "Introduction to Programming", 5)
+    # s3 = CourseAttempt("Peter Python", "Advanced Course in Programming", 0)
+
+    # for attempt in accepted([s1, s2, s3]):
+    #     print(attempt)
+    
+    # part 2
+    # s1 = CourseAttempt("Peter Python", "Introduction to Programming", 3)
+    # s2 = CourseAttempt("Olivia C. Objective", "Introduction to Programming", 5)
+    # s3 = CourseAttempt("Peter Python", "Introduction to AI", 3)
+    # s4 = CourseAttempt("Olivia C. Objective", "Data Structures and Algorithms", 3)
+
+    # for attempt in attempts_with_grade([s1, s2, s3, s4], 3):
+    #     print(attempt)
+
+    s1 = CourseAttempt("Peter Python", "Introduction to Programming", 3)
+    s2 = CourseAttempt("Olivia C. Objective", "Introduction to AI", 5)
+    s3 = CourseAttempt("Peter Python", "Introduction to AI", 0)
+    s4 = CourseAttempt("Jack Java", "Introduction to AI", 3)
+
+    for attempt in passed_students([s1, s2, s3, s4], "Introduction to AI"):
+        print(attempt)
 
 
